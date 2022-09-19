5b33d69ba3dfc63669ee1e5aab8843377b33b779
Caitano <josiasmartinscaitano@gmail.com>
2022-09-19 17:17:05 -0300

    feat: adicionado o estilo e template do menu

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ap-menu',
-  templateUrl: './menu.component.html'
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
 
