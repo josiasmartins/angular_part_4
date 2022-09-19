4ed42de52e102942249fe6e0b264285fd0711f55
Caitano <josiasmartinscaitano@gmail.com>
2022-09-19 20:46:21 -0300

    feat: exibindo o nome do usuário com stacktrace

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,19 +1,37 @@
-import { ErrorHandler, Injectable } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
+import { UserService } from '../../core/user/user.service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
+  constructor(
+    private injector: Injector
+  ) {}
+
   handleError(error: any) {
     console.log('passei pelo handler');
+
+    const location = this.injector.get(LocationStrategy);
+    const userService = this.injector.get(UserService);
+
+    const url = location instanceof PathLocationStrategy
+      ? location.path() : '';
+
+
+    const message = error.message ? error.message : error.toString();
     StackTrace
       .fromError(error)
       .then(stackFrames => {
         const stackAsString = stackFrames
           .map(sf => sf.toString())
           .join('\n');
-        console.log(error);
+
+        console.log(message);
         console.log(stackAsString);
+        console.log('oque será enviado no servidor');
+        console.log({ message, url, userName: userService.getUserName(), stack: stackAsString });
       })
     // throw error;
   }
